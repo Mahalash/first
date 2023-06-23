64bca0a5c7891f037cbeec47ff036148dedce49b
Mahalash <111861087+Mahalash@users.noreply.github.com>
2023-06-23 11:04:40 +0530

    Create new.py
    a positive message

---
+++ new.py
@@ -0,0 +1,10 @@
+#to daily confidence
+
+x = int(input("enter any number between 1 to 10:"))
+
+if x < 4:
+    print("This is going to be an amazing day.")
+elif x < 7:
+    print("You're gonna rock as always.")
+else:
+    print("You're amazing.")
